96b72f79c9e1705abbd93751d6fa723ff3f66949
websroc <songsparrow@163.com>
2018-04-21 13:01:32 +0800

    会员政策
    会员政策页面

--- pages/vippol/vippol.js
+++ pages/vippol/vippol.js
@@ -9,6 +9,25 @@ Page({
       threeti: "3. 客户关怀，新茶上市邮寄客户品尝",
       fourti: "4. 所有打7折",
       srced:"../../images/jiji.png"
-    }]
+    }],
+    arrayVip: [
+      {
+        mode: 'scaleToFill',
+        text: ' 专享红包',
+        src: '../../images/value1.png'
+      }, {
+        mode: 'scaleToFill',
+        text: '奖金激励',
+        src: '../../images/value2.png'
+      }, {
+        mode: 'scaleToFill',
+        text: '专属折扣',
+        src: '../../images/value1.png'
+      }, {
+        mode: 'scaleToFill',
+        text: '金币特权',
+        src: '../../images/value2.png'
+      }
+    ],
   }
 })
